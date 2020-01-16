e71e87afd921fe1b849ba5103e4ce3b279646698
ivanbrajovic <44555592+ivanbrajovic@users.noreply.github.com>
2020-01-16 16:15:38 +0100

    currency info componnt finished

--- src/components/CurrencyInfo/index.js
+++ src/components/CurrencyInfo/index.js
@@ -1,12 +1,61 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import SvgPie from './SvgPie'
 import './style.css'
 
 const res = (i, l) => (i / l) || "calculating" 
 
+const getPercentages = (i, x) =>  (x * 100 / i)
+const getPercentages_ = (i, x) => getPercentages(i, x) > 100
+  ? getPercentages(x, i)   
+  : getPercentages(i, x)
+const createColor = () => '#'+ Math.random().toString(16).slice(2, 8)
+const chooseCurrencyRelation = (i, x, y, z) => getPercentages(i, x) > 100 ? y : z
+
+// component
+
 const CurrencyInfo = () => {
   const currency = useSelector(state => state.json.base)
   const value = useSelector(state =>state.json.rate)
+  const values = {
+  'a': value?.rates[currency?.currencies[0].code],
+  'b': value?.rates['GBP'],
+  'v': value?.rates['USD']
+  }
+
+  const percentage = [
+    {
+      p: getPercentages(values.a, 1).toFixed(3),
+      pc: getPercentages_(values.a, 1).toFixed(3),
+      c: {
+        fill: createColor(),
+        stroke: createColor()
+      },
+      cur: chooseCurrencyRelation(values.a, values.v, "EUR", currency?.currencies[0].code),
+      curToCom: chooseCurrencyRelation(values.a, values.v, currency?.currencies[0].code, "EUR")
+    },
+    {
+      p: getPercentages(values.a, values.b).toFixed(3),
+      pc: getPercentages_(values.a, values.b).toFixed(3),
+      c: {
+        fill: createColor(),
+        stroke: createColor()
+      },
+      cur: chooseCurrencyRelation(values.a, values.v, "GBP", currency?.currencies[0].code),
+      curToCom: chooseCurrencyRelation(values.a, values.v, currency?.currencies[0].code, "GBP")
+    },
+    {
+      p: getPercentages(values.a, values.v).toFixed(3),
+      pc: getPercentages_(values.a, values.v).toFixed(3),
+      c: {
+        fill: createColor(),
+        stroke: createColor()
+      },
+      cur: chooseCurrencyRelation(values.a, values.v, "USD", currency?.currencies[0].code),
+      curToCom: chooseCurrencyRelation(values.a, values.v, currency?.currencies[0].code, "USD")
+    }
+  ]
+
   return (
     <div className="currency-info-component">
       <h1>Currency Info</h1>
@@ -16,11 +65,19 @@ const CurrencyInfo = () => {
       </h2>
       <h4>Country: <span>{currency?.name}</span> </h4>
       <h4>Relative to EUR:&nbsp;
-      <span>{value?.rates[currency?.currencies[0].code]}</span></h4>
+      <span>{values.a}
+        <span className="percentag">{percentage[0].p +"%"}</span></span></h4>
       <h4>Relative to GBP:&nbsp;
-      <span>{res(value?.rates[currency?.currencies[0].code], value?.rates['GBP'])}</span></h4>
+      <span>{res(values.a, values.b )}
+        <span className="percentag"> {percentage[1].p+"%"}</span></span></h4>
       <h4>Relative to USD:&nbsp;
-      <span>{res(value?.rates[currency?.currencies[0].code], value?.rates['USD'])}</span></h4>   
+      <span>{res(values.a, values.v )}
+        <span className="percentag">{percentage[2].p+"%"}</span></span></h4>
+        <div className="svg-charts">
+          {percentage.map(item => (console.log(item, item.p, item.c, item.c.fill),
+            <SvgPie per={item.pc} fill={item.c.fill} stroke={item.c.stroke} c={item.cur} comp={item.curToCom}/>
+          ))}   
+        </div>
      </div>
   )
 }
